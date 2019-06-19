e1e26f3d8581a72558c8a0a9ec44e8d64a4cef4e
Philip <philip.faure13@gmail.com>
2019-06-19 20:13:47 +0200

    \\rebuilding

--- R/copySpByYear.R
+++ R/copySpByYear.R
@@ -2,12 +2,13 @@
 #'
 #' Copy all images from a directory which has a certain metadata tag and from a certain year as specified by the user. This function is very much similar to the getSpeciesImages function from the camtrapR package except you can specify which year the images are from and it can use Windows Live Gallery tags as well as digiKam tags.
 #'
+#'
 #' @param inDir character. The directory where all the images are saved in.
 #' @param newDir character. The directory where you would like to copy your images to.
 #' @param exifTag character. The name of the species tag column. For images tagged in Windows Live Gallery use "Subject", and for images tagged in digiKam use "HierarchicalSubject" as the input argumenets. Actually, now after I have cheanged the code to include digiKam, I think "Subject" might just work for both... *palm to face
 #' @param species character. The species tag as tagged in Windows Live Gallery.
 #' @param year integer. The year from which you would like the images from.
-#' @return Copies all the images according to the specified arguments (species, year) to a new directory as chosen by the user.
+#' @return Copies all images according to the specified arguments (species, year) to a new directory as chosen by the user.
 #'
 #' @export
 #'
@@ -48,4 +49,5 @@ copySpByYear <- function (inDir, newDir, exifTag, species, year)
   message("...........................")
   message(".....Rust Never Sleeps.....")
   message("...........................")
+
 }
