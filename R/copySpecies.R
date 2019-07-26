532c21d4caf27127c37e013eacd4859460f33274
Philip Faure <philipfaure@Philips-MacBook-Pro.local>
2019-07-26 17:32:47 +0200

    added copySpecies updated imgRename played around procrastinate...big time!

---
+++ R/copySpecies.R
@@ -0,0 +1,76 @@
+#' Copy photos of specific species
+#'
+#' Copy all images from a specified directory which has a certain metadata species tag to a specified directory set by user. This function is very much similar to the 'getSpeciesImages' function from the camtrapR package except you can use Windows Live Gallery tags as well as digiKam tags.
+#'
+#'
+#' @param inDir character. The directory where all the images are saved in.
+#' @param newDir character. The directory where you would like to copy your images to.
+#' @param exifTag character. The name of the species tag column. For images tagged in Windows Live Gallery use "Subject", and for images tagged in digiKam use "HierarchicalSubject" as the input argumenets. Actually, now after I have cheanged the code to include digiKam, I think "Subject" might just work for both... *palm to face
+#' @param species character. The species tag as tagged in Windows Live Gallery.
+#'
+#'
+#' @return Copies all images according to the specified arguments (species, year) to a new directory as chosen by the user.
+#'
+#' @export
+#'
+#' @author Philip Faure, \email{philip.faure13@gmail.com}
+#' @keywords Copy species
+#'
+#'
+#' @examples
+#' copySpByYear(inDir   = "/Users/philipfaure/Desktop/testSample",
+#'              newDir  = "/Users/philipfaure/Desktop/testOutput",
+#'              exifTag = "Subject",
+#'              species = "Leopard")
+#'
+#' @export
+#'
+copySpecies <- function (inDir, newDir, exifTag, species)
+{
+  start <- Sys.time()
+  if (Sys.info()[["sysname"]] == "Windows") {
+    message("...Looking for tagged images to copy to newDir.")
+    shell(
+      paste(
+        'exiftool -r -o .',
+        ' ',
+        '"-Filename=',
+        newDir,
+        '/%f%-c%E',
+        '" ',
+        '-if "$',
+        exifTag,
+        '=~/',
+        species,
+        '/i',
+        inDir,
+        sep = ""
+      )
+    )
+  }
+  else {
+    message("...Looking for tagged images to copy to newDir.")
+    system(
+      paste(
+        "exiftool -r -o . ",
+        "'-Filename=",
+        newDir,
+        "/%f%-c%E",
+        "' -if '$",
+        exifTag,
+        "=~/",
+        species,
+        "/i' ",
+        inDir,
+        sep = ""
+      )
+    )
+  }
+
+  print(difftime(Sys.time(), start))
+
+  message("...........................")
+  message("....'Rust Never Sleeps'....")
+  message("...........................")
+
+}
--- R/imgRename.R
+++ R/imgRename.R
@@ -1,8 +1,8 @@
 #' Rename JPG images
 #'
-#' Renames images according to YYYYmmdd_HHMMSS.jpg. It is better to just use the renaming function in the package 'camtrapR instead. It is better tested and also adds the station and camera column to the file name. This function I made is just for the fun of it.
+#' Renames images according to YYYYmmdd_HHMMSS.jpg. It is better to just use the renaming function in the package 'camtrapR' instead. It is better tested and also adds the station and camera column to the file name. This function I made is just for the fun of it.
+#' @param inDir character. The directory where your images are saved. Make sure you are working on a back up of your data in case something goes wrong. If you are still deciding on using this function, then please work on a copy of your data and not the raw data so that if something happens you still have a backup.
 #'
-#' @param inDir character. The directory where your images are saved. Make sure you are working on a back up of your data in case something goes wrong.
 #'
 #' @author Philip Faure, \email{philip.faure13@gmail.com}
 #' @keywords image rename
@@ -14,15 +14,40 @@
 
 imgRename <- function (inDir)
 {
-time <- Sys.time()
+  time <- Sys.time()
+
+  message("Have you made a copy of your data? Please always work on a copy of your images so that if something goes wrong you still have a backup.")
+
+  stopifnot(length(inDir) == 1)
+  if (!dir.exists(inDir))
+    stop("The directory you have specified as the 'inDir' argument does not seem to exist. Please check that you have correctly typed the directory where your images are. ", inDir, call. = FALSE)
+  if (Sys.which("exiftool") == "")
+    stop("You're system does not seem have Exiftool installed. Please download from https://www.sno.phy.queensu.ca/~phil/exiftool/ and install. For Windows operating systems you need to [1] rename the downloaded file to Exiftool.exe and [2] place it in your Windows folder (i.e. C:/Windows). For MacOS you only need to download and install, and you should be good to go.", call. = FALSE)
+
   if (Sys.info()[["sysname"]] == "Windows") {
-message(".......")
-     shell(paste("exiftool -r -d %Y%m%d_%H%M%S%%-c.%%e '-filename<CreateDate'", inDir, sep = ""))
+    message("...your images are now being renamed.")
+    shell(
+      paste(
+        "exiftool -r -d",
+        "%Y%m%d_%H%M%S%%-c.%%e '-filename<DateTimeOriginal'",
+        inDir,
+        sep = " "
+      )
+    )
+
   }
-else {
-message(".......")
-    system(paste("exiftool -r -d %Y%m%d_%H%M%S%%-c.%%e '-filename<CreateDate'", inDir, sep = ""))
+  else {
+    message("...your images are now being renamed.")
+    system(
+      paste(
+        "exiftool -r -d",
+        "%Y%m%d_%H%M%S%%-c.%%e '-filename<DateTimeOriginal'",
+        inDir,
+        sep = " "
+      )
+    )
   }
-  comp.time <- Sys.time() - time
-print(comp.time)
+
+  print(difftime(Sys.time(), time))
+  message("All done!")
 }
